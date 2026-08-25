d31722aacd7d71b44f563cb72225c973e35b50a4
carlosao24 <carloslopez21trabajos@gmail.com>
2026-08-24 19:58:50 -0500

    Creacion de clase Dimensiones

---
+++ src/com/ecoenvio/modelo/Dimensiones.java
@@ -0,0 +1,41 @@
+package com.ecoenvio.modelo;
+
+public class Dimensiones {
+    private double alto;
+    private double ancho;
+    private double largo;
+
+    public Dimensiones(double alto, double ancho, double largo) {
+        this.alto = alto;
+        this.ancho = ancho;
+        this.largo = largo;
+    }
+
+    public double getAlto() {
+        return alto;
+    }
+
+    public void setAlto(double alto) {
+        this.alto = alto;
+    }
+
+    public double getAncho() {
+        return ancho;
+    }
+
+    public void setAncho(double ancho) {
+        this.ancho = ancho;
+    }
+
+    public double getLargo() {
+        return largo;
+    }
+
+    public void setLargo(double largo) {
+        this.largo = largo;
+    }
+
+    public double calcularVolumen() {
+        return alto * ancho * largo;
+    }
+}
